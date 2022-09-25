dada9b54ba121271940e073f00d17f67c3d79fb3
Suchona013 <taslimakabir013@gmail.com>
2022-09-25 06:09:36 +0600

    upload

---
+++ report.js
@@ -0,0 +1,16 @@
+const newman = require('newman');
+
+newman.run({
+	collection: require('./collection/collection.json'),
+
+	reporters: 'htmlextra',
+	iterationCount: 1,
+	reporter: {
+		htmlextra: {
+			export: './Reports/report.html',
+		}
+	}
+}, function(err){
+	if(err) {throw err;}
+	console.log('collection run complete!');
+});
